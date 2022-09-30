926175d2543dfa03ea69d470f228acd50edb6b27
Laure-Di <masson.laure.l@gmail.com>
2022-09-30 13:29:16 +0200

    finish of script wp

--- srcs/requirements/wordpress/wp.sh
+++ srcs/requirements/wordpress/wp.sh
@@ -1,11 +1,13 @@
 #On check si le fichier wp déjà download, attention a stocker dans vol wp
-
-cd /var/www/html
-
-wp core download --allow-root
-wp config create --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=mariadb --dbcharset="utf8" --dbcollate="utf8_general_ci"
-wp core install --url=lmasson.42.fr --title="LEMP" --admin_user=${MYSQL_ADMIN} --admin_password=${MYSQL_ADMIN_PASSWORD} --skip-email
-wp user create user ${USER_WP} --role=author --user_pass=${USER_WP_PASS}
-wp theme install twentysixteen --activate
+if [ ! -f /var/www/html/wp-config.php ]; then
+ 	cd /var/www/html/
+	wp core download --allow-root
+	wp config create --dbname=${DB_WORDPRESS} --dbuser=${DB_USER} --dbpass=${DB_USER_PASSWORD} --dbhost=mariadb --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
+	wp core install --url=${WP_URL} --title="${WP_TITLE}" --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_MAIL} --skip-email --allow-root
+	wp user create ${WP_USER} ${WP_USER_MAIL} --role=author --user_pass=${WP_USER_PASSWORD} --allow-root
+	wp theme install "teluro" --activate --allow-root
+	else
+		echo "wordpress already created"
+fi
 
 php-fpm7.3 -F -R
