39fcd59b32afd24f79d265a836135cbed40e6b21
Laure MASSON <lmasson@e1r10p16.clusters.42paris.fr>
2022-09-30 12:19:42 +0200

    call of php-fpm in script and change to .env bvar

--- srcs/requirements/wordpress/wp.sh
+++ srcs/requirements/wordpress/wp.sh
@@ -1,7 +1,11 @@
 #On check si le fichier wp déjà download, attention a stocker dans vol wp
 
+cd /var/www/html
+
 wp core download --allow-root
-wp config create --dbname=wp_wordpress --dbuser=admin --dbpass=coucou --dbhost=mariadb --dbcharset="utf8" --dbcollate="utf8_general_ci"
-wp core install --url=lmasson.42.fr --title="LEMP" --admin_user=lmasson --admin_password=hello --skip-email
-wp user create user user@student.42.fr --role=author --user_pass=yolo
+wp config create --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=mariadb --dbcharset="utf8" --dbcollate="utf8_general_ci"
+wp core install --url=lmasson.42.fr --title="LEMP" --admin_user=${MYSQL_ADMIN} --admin_password=${MYSQL_ADMIN_PASSWORD} --skip-email
+wp user create user ${USER_WP} --role=author --user_pass=${USER_WP_PASS}
 wp theme install twentysixteen --activate
+
+php-fpm7.3 -F -R
