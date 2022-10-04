cc69e42c3331b216973c3b3de502b17638d71f05
Laure MASSON <lmasson@e1r5p17.clusters.42paris.fr>
2022-10-04 16:36:30 +0200

    debug

--- srcs/requirements/mariadb/setup_script.sh
+++ srcs/requirements/mariadb/setup_script.sh
@@ -26,7 +26,7 @@ EOF
 	done
 	mysql -u root -e "CREATE DATABASE ${MYSQL_DATABAS};"
 	mysql -u root -e "SET GLOBAL general_log=1; SET GLOBAL general_log_file='mariadb.log';"
-	mysql -u root -e "CREATE USER ${MYSQL_ADMIN}@'localhost' IDENTIFIED BY ${MYSQL_ADMIN_PASSWORD};"
+	mysql -u root -e "CREATE USER ${MYSQL_ADMIN}@'localhost' IDENTIFIED BY '${MYSQL_ADMIN_PASSWORD}';"
 	mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO ${MYSQL_USER}@'localhost';"
 	mysql -u root -e "CREATE USER ${MYSQL_USER}@'localhost' IDENTIFIED BY ${MYSQL_PASSWORD}; GRANT ALL ON wp_wordpress.* TO ${MYSQL_USER}@'localhost'"
 	mysql -u root -e "FLUSH PRIVILEGES;"
