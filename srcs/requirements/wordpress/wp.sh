1cd63be04af324d45a597c4b1e31d4c4bb27955b
Laure MASSON <lmasson@e1r5p17.clusters.42paris.fr>
2022-10-04 19:04:52 +0200

    debug

--- srcs/requirements/wordpress/wp.sh
+++ srcs/requirements/wordpress/wp.sh
@@ -2,6 +2,9 @@
 if [ ! -f /var/www/html/wp-config.php ]; then
  	cd /var/www/html/
 	wp core download --allow-root
+	until mysqladmin -hmariadb -u${MYSQL_USER} p${MYSQL_PASSWORD} ping;do
+	sleep 2
+	done
 	wp config create --dbname=${MYSQL_DB} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=mariadb --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
 	wp core install --url=${URL} --title="${TITLE}" --admin_user=${ADMIN_WP} --admin_password=${ADMIN_PASSWD} --admin_email=${ADMIN_EMAIL} --skip-email --allow-root
 	wp user create ${USER_WP} ${USER_MAIL_WP} --role=author --user_pass=${USER_WP_PASS} --allow-root
