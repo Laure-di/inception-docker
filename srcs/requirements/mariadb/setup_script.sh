e3102747be4aa835e978e9960797b85a8e95d5ad
Laure MASSON <lmasson@e1r5p17.clusters.42paris.fr>
2022-10-04 18:21:51 +0200

    debug

--- srcs/requirements/mariadb/setup_script.sh
+++ srcs/requirements/mariadb/setup_script.sh
@@ -6,11 +6,8 @@ chmod o+w /dev/stderr
 mysql_install_db
 service mysql start
 
-if [ -d /var/lib/mysql/wp_wordpress ]; then
-	echo "The database already exist!"
-else
-mysql_secure_installation
-<<EOF
+if [ ! -d /var/lib/mysql/wp_wordpress ]; then
+mysql_secure_installation<<EOF
 
 y
 secret
@@ -21,15 +18,17 @@ y
 y
 EOF
 
-	until mysqladmin ping;do
-		sleep 2
-	done
-	mysql -u root -e "CREATE DATABASE ${MYSQL_DATABAS};"
-	mysql -u root -e "SET GLOBAL general_log=1; SET GLOBAL general_log_file='mariadb.log';"
-	mysql -u root -e "CREATE USER '${MYSQL_ADMIN}'@'localhost' IDENTIFIED BY '${MYSQL_ADMIN_PASSWORD}';"
-	mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO ${MYSQL_USER}@'localhost';"
-	mysql -u root -e "CREATE USER '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}'; GRANT ALL ON wp_wordpress.* TO '${MYSQL_USER}'@'localhost'"
-	mysql -u root -e "FLUSH PRIVILEGES;"
-	echo "Database created!"
-	fi
+until mysqladmin ping;do
+sleep 2
+done
+mysql -u root -e "CREATE DATABASE ${MYSQL_DATABAS};"
+mysql -u root -e "SET GLOBAL general_log=1; SET GLOBAL general_log_file='mariadb.log';"
+mysql -u root -e "CREATE USER '${MYSQL_ADMIN}'@'localhost' IDENTIFIED BY '${MYSQL_ADMIN_PASSWORD}';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO ${MYSQL_USER}@'localhost';"
+mysql -u root -e "CREATE USER '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}'; GRANT ALL ON wp_wordpress.* TO '${MYSQL_USER}'@'localhost'"
+mysql -u root -e "FLUSH PRIVILEGES;"
+echo "Database created!"
+else
+echo "The database already exist!"
+fi
 service mysql stop
