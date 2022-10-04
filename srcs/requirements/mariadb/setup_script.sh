f2a82d1c3e1deb1281aa433e3f1f3230653ad491
Laure MASSON <lmasson@e1r5p17.clusters.42paris.fr>
2022-10-04 18:28:09 +0200

    debug

--- srcs/requirements/mariadb/setup_script.sh
+++ srcs/requirements/mariadb/setup_script.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-chmod o+w /dev/stdout
-chmod o+w /dev/stderr
-
 mysql_install_db
 service mysql start
 
@@ -21,10 +18,9 @@ EOF
 until mysqladmin ping;do
 sleep 2
 done
-mysql -u root -e "CREATE DATABASE ${MYSQL_DATABAS};"
-mysql -u root -e "SET GLOBAL general_log=1; SET GLOBAL general_log_file='mariadb.log';"
+mysql -u root -e "CREATE DATABASE '${MYSQL_DATABAS}';"
 mysql -u root -e "CREATE USER '${MYSQL_ADMIN}'@'localhost' IDENTIFIED BY '${MYSQL_ADMIN_PASSWORD}';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO ${MYSQL_USER}@'localhost';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'localhost';"
 mysql -u root -e "CREATE USER '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}'; GRANT ALL ON wp_wordpress.* TO '${MYSQL_USER}'@'localhost'"
 mysql -u root -e "FLUSH PRIVILEGES;"
 echo "Database created!"
