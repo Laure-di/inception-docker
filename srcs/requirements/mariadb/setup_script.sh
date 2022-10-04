a38c777de703062ee68209c787214c3444a2e553
Laure MASSON <lmasson@e1r5p17.clusters.42paris.fr>
2022-10-04 18:33:29 +0200

    debug

--- srcs/requirements/mariadb/setup_script.sh
+++ srcs/requirements/mariadb/setup_script.sh
@@ -18,7 +18,7 @@ EOF
 until mysqladmin ping;do
 sleep 2
 done
-mysql -u root -e "CREATE DATABASE '${MYSQL_DATABAS}';"
+mysql -u root -e "CREATE DATABASE ${MYSQL_DATABASE};"
 mysql -u root -e "CREATE USER '${MYSQL_ADMIN}'@'localhost' IDENTIFIED BY '${MYSQL_ADMIN_PASSWORD}';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'localhost';"
 mysql -u root -e "CREATE USER '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}'; GRANT ALL ON wp_wordpress.* TO '${MYSQL_USER}'@'localhost'"
