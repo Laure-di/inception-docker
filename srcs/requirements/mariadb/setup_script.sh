52873e41b379a1c51037c101a6f594aaacca9335
Laure-Di <masson.laure.l@gmail.com>
2022-10-05 11:52:34 +0200

    debug

--- srcs/requirements/mariadb/setup_script.sh
+++ srcs/requirements/mariadb/setup_script.sh
@@ -18,11 +18,11 @@ if [ ! -d /var/lib/mysql/${MYSQL_DATABASE} ]; then
 until mysqladmin ping;do
 sleep 2
 done
-mysql -u root -e "CREATE DATABASE ${MYSQL_DATABASE};"
-mysql -u root -e "CREATE USER '${MYSQL_ADMIN}'@'localhost' IDENTIFIED BY '${MYSQL_ADMIN_PASSWORD}';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_ADMIN}'@'localhost';"
-mysql -u root -e "CREATE USER '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}'; GRANT ALL ON wp_wordpress.* TO '${MYSQL_USER}'@'localhost'"
-mysql -u root -e "FLUSH PRIVILEGES;"
+mysql -uroot -e "CREATE DATABASE ${MYSQL_DATABASE};"
+mysql -uroot -e "CREATE USER '${MYSQL_ADMIN}'@'localhost' IDENTIFIED BY '${MYSQL_ADMIN_PASSWORD}';"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_ADMIN}'@'localhost';"
+mysql -uroot -e "CREATE USER '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}'; GRANT ALL ON wp_wordpress.* TO '${MYSQL_USER}'@'localhost'"
+mysql -uroot -e "FLUSH PRIVILEGES;"
 echo "Database created!"
 else
 echo "The database already exist!"
