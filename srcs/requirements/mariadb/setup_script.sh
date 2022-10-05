4bba94ad26963fb3e24bcba472796e3a1e303f33
Laure-Di <masson.laure.l@gmail.com>
2022-10-05 11:54:36 +0200

    debug

--- srcs/requirements/mariadb/setup_script.sh
+++ srcs/requirements/mariadb/setup_script.sh
@@ -4,17 +4,19 @@ mysql_install_db
 
 /etc/init.d/mysql start
 
-if [ ! -d /var/lib/mysql/${MYSQL_DATABASE} ]; then
-#mysql_secure_installation <<EOF
-#
-#Y
-#hellocoucou
-#hellocoucou
-#Y
-#Y
-#Y
-#Y
-#EOF
+if [  -d /var/lib/mysql/${MYSQL_DATABASE} ]; then
+echo "The database already exist"
+else
+mysql_secure_installation <<EOF
+
+Y
+hellocoucou
+hellocoucou
+Y
+Y
+Y
+Y
+EOF
 until mysqladmin ping;do
 sleep 2
 done
@@ -24,8 +26,6 @@ mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_ADMIN}'@'localhost';"
 mysql -uroot -e "CREATE USER '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}'; GRANT ALL ON wp_wordpress.* TO '${MYSQL_USER}'@'localhost'"
 mysql -uroot -e "FLUSH PRIVILEGES;"
 echo "Database created!"
-else
-echo "The database already exist!"
 fi
 /etc/init.d/mysql stop
 exec "$@"
